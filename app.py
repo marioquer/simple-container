b1fcb3213b96f1622a6bc0dfcefa411ff5b63306
Mario Que <marioquer@hotmail.com>
2019-11-19 00:12:48 +0000

    establish framework

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, escape, request
+import os
+import sys
+import manager
+
+app = Flask(__name__)
+manager = None
+
+'''
+Post a new config
+'''
+@app.route('/config', methods=['POST'])
+def image_config():
+    return manager.add_config(request.data)
+
+
+'''
+List config information
+'''
+@app.route('/cfginfo', methods=['GET'])
+def list_config():
+    return manager.list_config()
+
+
+'''
+Launch a new container
+'''
+@app.route('/launch', methods=['POST'])
+def launch():
+    return manager.launch(request.data)
+
+
+'''
+List container instance
+'''
+@app.route('/list', methods=['GET'])
+def list_container():
+    return manager.list_container
+
+
+'''
+Destroy container instance
+'''
+@app.route('/destroy/<string:pk>', methods=['DELETE'])
+def destroy_container(pk):
+    return manager.destroy_container(pk)
+
+
+'''
+Destroy all container instances
+'''
+@app.route('/destroyall', methods=['DELETE'])
+def destroy_all():
+    return manager.destroy_all()
+
+
+if __name__ == "__main__":
+    manager = manager.Manager(sys.argv)
+    app.run(host=sys.argv[1], port=sys.argv[2], threaded=False)
---
+++ manager.py
@@ -0,0 +1,29 @@
+
+
+class Manager():
+    def __init__(self, argv):
+        pass
+
+    def add_config(self, argv):
+        '''
+        configs store in configs/
+        '''
+        pass
+
+    def list_config(self):
+        pass
+
+    def launch(self, argv):
+        '''
+        container rootdirs store in containers/
+        '''
+        pass
+
+    def list_container(self):
+        pass
+
+    def destroy_container(self, name):
+        pass
+
+    def destroy_all(self):
+        pass
